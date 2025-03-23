68df9965df745b607e8aae9564d9398dea4fa568
TCRONEB HACKX <thelastcroneb@gmail.com>
2025-03-23 20:31:59 +0200

    Update sample_config.py

--- SophiaBot/sample_config.py
+++ SophiaBot/sample_config.py
@@ -16,10 +16,10 @@ class Config(object):
 
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1725052009  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    TOKEN = "7215903425:AAEKVxJbnq429tf9xEGyAhuKtJYdOZcFCGY"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 7080079152  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "dihanrandila" 
-    SUPPORT_CHAT = "dihan_official"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = "tcronebhackx"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001392103597
     )  # Prints any new group the bot is added to, prints just the name and ID.
